638abe6fd517bab2de2b7f5310acc4330ba58e6e
dohyung97022 <dohyung97022@gmail.com>
2021-09-16 19:49:46 +0900

    [#6] fix : body 를 byte[] 로 받기, header 를 map 으로 변경

--- src/main/java/com/nt/ntp/util/http/HttpUtils.java
+++ src/main/java/com/nt/ntp/util/http/HttpUtils.java
@@ -1,27 +1,29 @@
 package com.nt.ntp.util.http;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.NonNull;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
+import java.util.List;
 
 public class HttpUtils {
     public static Response connect(@NonNull Request request) throws IOException {
         // Request
         HttpURLConnection urlConnection = (HttpURLConnection) request.getUri().toURL().openConnection();
         urlConnection.setRequestMethod(request.getRequestMethod().toString());
-        if ( request.getHeaders() != null)
-            request.getHeaders().forEach(header -> urlConnection.setRequestProperty(header.getKey(), header.getValues()));
+        if (request.getHeader() != null)
+            request.getHeader().forEach((key, values) -> urlConnection.setRequestProperty(key, joinHeaderValues(values)));
 
         // Response
-        JsonNode body = new ObjectMapper().readTree(urlConnection.getInputStream());
         return new Response(
                 urlConnection.getResponseCode(),
                 urlConnection.getResponseMessage(),
                 urlConnection.getHeaderFields(),
-                body
+                urlConnection.getInputStream().readAllBytes()
         );
+    }
+
+    public static String joinHeaderValues(List<String> headerValues){
+        return String.join(";", headerValues);
     }
 }
--- src/main/java/com/nt/ntp/util/http/Request.java
+++ src/main/java/com/nt/ntp/util/http/Request.java
@@ -6,11 +6,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Map;
 
 @Getter
 @AllArgsConstructor
 public class Request {
     private final URI uri;
     private final RequestMethod requestMethod;
-    private final List<Header> headers;
+    private final Map<String, List<String>> header;
 }
