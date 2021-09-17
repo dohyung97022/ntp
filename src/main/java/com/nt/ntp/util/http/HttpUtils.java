12ef9f9f1b5f73e38241e05d8a58c6ac188fba18
dohyung97022 <dohyung97022@gmail.com>
2021-09-18 02:18:14 +0900

    [#19] test : 경우에 맞는 exception 추가

--- src/main/java/com/nt/ntp/util/http/HttpUtils.java
+++ src/main/java/com/nt/ntp/util/http/HttpUtils.java
@@ -1,14 +1,19 @@
 package com.nt.ntp.util.http;
 
+import com.nt.ntp.exception.InvalidResponseException;
 import lombok.NonNull;
+import org.springframework.http.HttpStatus;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
+import java.security.InvalidParameterException;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class HttpUtils {
+    private static final List<Integer> errorCodes = List.of(HttpStatus.BAD_REQUEST.value(), HttpStatus.NOT_FOUND.value());
+
     public static Response connect(@NonNull Request request) throws IOException {
         // Request
         HttpURLConnection urlConnection = (HttpURLConnection) request.getUri().toURL().openConnection();
@@ -17,25 +22,31 @@ public class HttpUtils {
             request.getHeader().forEach((key, values) -> urlConnection.setRequestProperty(key, joinHeaderValues(values)));
 
         // Response
-        return new Response(
+        Response response = new Response(
                 urlConnection.getResponseCode(),
                 urlConnection.getResponseMessage(),
                 urlConnection.getHeaderFields(),
-                urlConnection.getInputStream().readAllBytes()
-        );
+                (errorCodes.contains(urlConnection.getResponseCode()))
+                        ? urlConnection.getErrorStream().readAllBytes() : urlConnection.getInputStream().readAllBytes());
+
+        // Check
+        if (HttpUtils.errorCodes.contains(urlConnection.getResponseCode()))
+            throw new InvalidResponseException(response);
+
+        return response;
     }
 
-    public static String joinHeaderValues(List<String> headerValues){
+    public static String joinHeaderValues(List<String> headerValues) {
         return String.join(";", headerValues);
     }
 
     public static String getAccessTokenFromSetCookieValue(String setCookieValue) {
-        Pattern pattern = Pattern.compile("Authorization=([^;^ ]+)");
-        Matcher matcher = pattern.matcher(setCookieValue);
+        String pattern = "Authorization=([^;^ ]+)";
+        Matcher matcher = Pattern.compile("Authorization=([^;^ ]+)").matcher(setCookieValue);
         if (matcher.find()) {
             return matcher.group(1);
         } else {
-            throw new IllegalArgumentException("setCookie 가 패턴에 맞지 않습니다.");
+            throw new InvalidParameterException("setCookie " + setCookieValue + " 가 패턴 " + pattern + " 에 맞지 않습니다.");
         }
     }
 }
