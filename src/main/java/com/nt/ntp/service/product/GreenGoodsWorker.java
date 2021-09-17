7302742f80736d82cd3645505e8e73ceded56bbe
dohyung97022 <dohyung97022@gmail.com>
2021-09-17 21:46:15 +0900

    [#15] feature : getPrice 를 Worker 에 구현

--- src/main/java/com/nt/ntp/service/product/GreenGoodsWorker.java
+++ src/main/java/com/nt/ntp/service/product/GreenGoodsWorker.java
@@ -1,5 +1,6 @@
 package com.nt.ntp.service.product;
 
+import com.nt.ntp.dto.response.GetPriceResponseDto;
 import com.nt.ntp.util.http.HttpUtils;
 import com.nt.ntp.util.http.Request;
 import com.nt.ntp.util.http.Response;
@@ -16,6 +17,7 @@ import java.util.Map;
 
 public class GreenGoodsWorker implements GreenGoodsWorkerImpl{
     protected static final String ACCESS_TOKEN_PATH = "token";
+    private static final String GET_PRICE_QUERY_NAME = "name";
     protected String GOODS_PATH;
     protected String URL;
 
@@ -33,5 +35,15 @@ public class GreenGoodsWorker implements GreenGoodsWorkerImpl{
         response.getJsonNodeBody().elements().forEachRemaining(jsonNode -> goodsList.add(jsonNode.asText()));
 
         return goodsList;
+    }
+
+    public GetPriceResponseDto getPrice(String authorization, String name) throws IOException{
+        URI uri = UriComponentsBuilder
+                .fromUriString(URL).path(GOODS_PATH).queryParam(GET_PRICE_QUERY_NAME, name).encode().build().toUri();
+        Map<String, List<String>> header = new Hashtable<>();
+        header.put(HttpHeaders.AUTHORIZATION, List.of(authorization));
+        Response response = HttpUtils.connect(new Request(uri, RequestMethod.GET, header));
+
+        return new GetPriceResponseDto(response.getJsonNodeBody());
     }
 }
