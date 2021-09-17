4f1b86f05ee6abec50e5945446355a47f2b7c98f
dohyung97022 <dohyung97022@gmail.com>
2021-09-17 12:57:08 +0900

    [#11] feature : GreenGoodsRestController 에서 getGoods 받기

--- src/main/java/com/nt/ntp/controller/GreenGoodsRestController.java
+++ src/main/java/com/nt/ntp/controller/GreenGoodsRestController.java
@@ -1,14 +1,17 @@
 package com.nt.ntp.controller;
 
+import com.nt.ntp.dto.response.GetGoodsResponseDto;
 import com.nt.ntp.service.GreenGoodsService;
 import com.nt.ntp.util.enums.GreenGoods;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 @RequestMapping("api/green-goods")
@@ -21,5 +24,12 @@ public class GreenGoodsRestController {
     public ResponseEntity<?> setCookieAsToken(@RequestParam("type") GreenGoods type, HttpServletResponse response) throws IOException {
         greenGoodsService.setCookieToAccessToken(type, response);
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/product")
+    public ResponseEntity<GetGoodsResponseDto> getGoods
+            (@RequestParam("type") GreenGoods type, @CookieValue(HttpHeaders.AUTHORIZATION) String token) throws IOException {
+        List<String> products = greenGoodsService.getGoods(type, token);
+        return ResponseEntity.ok().body(new GetGoodsResponseDto(products));
     }
 }
