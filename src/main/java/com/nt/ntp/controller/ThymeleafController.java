89693021d211529b9ad30cf2f1d5298f1f2a8b4b
dohyung97022 <dohyung97022@gmail.com>
2021-09-17 10:34:07 +0900

    [#9] feature : ThymeleafController, index.html 생성

---
+++ src/main/java/com/nt/ntp/controller/ThymeleafController.java
@@ -0,0 +1,12 @@
+package com.nt.ntp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ThymeleafController {
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+}
