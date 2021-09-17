9f6e8ef81c0082c88719f7ee65da85f0bf170089
dohyung97022 <dohyung97022@gmail.com>
2021-09-17 14:54:49 +0900

    [#13] refactor : HttpUtils 로 getAccessTokenFromSetCookieValue 옮기기

--- src/main/java/com/nt/ntp/util/http/HttpUtils.java
+++ src/main/java/com/nt/ntp/util/http/HttpUtils.java
@@ -5,6 +5,8 @@ import lombok.NonNull;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class HttpUtils {
     public static Response connect(@NonNull Request request) throws IOException {
@@ -25,5 +27,15 @@ public class HttpUtils {
 
     public static String joinHeaderValues(List<String> headerValues){
         return String.join(";", headerValues);
+    }
+
+    public static String getAccessTokenFromSetCookieValue(String setCookieValue) {
+        Pattern pattern = Pattern.compile("Authorization=([^;^ ]+)");
+        Matcher matcher = pattern.matcher(setCookieValue);
+        if (matcher.find()) {
+            return matcher.group(1);
+        } else {
+            throw new IllegalArgumentException("setCookie 가 패턴에 맞지 않습니다.");
+        }
     }
 }
