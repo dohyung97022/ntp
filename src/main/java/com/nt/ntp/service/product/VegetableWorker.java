2eb36d8160e5559914065f40a224c086e88770e8
dohyung97022 <dohyung97022@gmail.com>
2021-09-17 14:52:58 +0900

    [#13] refactor : product 인 GreenGoods/Fruit/Vegetable Worker 생성

---
+++ src/main/java/com/nt/ntp/service/product/VegetableWorker.java
@@ -0,0 +1,26 @@
+package com.nt.ntp.service.product;
+
+import com.nt.ntp.util.http.HttpUtils;
+import com.nt.ntp.util.http.Request;
+import com.nt.ntp.util.http.Response;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.io.IOException;
+import java.net.URI;
+
+public class VegetableWorker extends GreenGoodsWorker implements GreenGoodsWorkerImpl{
+
+    public VegetableWorker(String url) {
+        super.URL = url;
+        super.GET_GOODS_PATH = "item";
+    }
+
+    @Override
+    public String getAccessToken() throws IOException {
+        URI uri = UriComponentsBuilder.fromUriString(URL).path(ACCESS_TOKEN_PATH).build().toUri();
+        Response response = HttpUtils.connect(new Request(uri));
+        String setCookieValue = HttpUtils.joinHeaderValues(response.getHeader().get(HttpHeaders.SET_COOKIE));
+        return HttpUtils.getAccessTokenFromSetCookieValue(setCookieValue);
+    }
+}
