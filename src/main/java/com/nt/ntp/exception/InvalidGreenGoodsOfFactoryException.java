98fe4c299722a123fa248d7f9c80cb112a5a7cf2
dohyung97022 <dohyung97022@gmail.com>
2021-09-18 02:23:09 +0900

    [#19] docs : 할당되지 않았거나 -> null 이거나로 수정

--- src/main/java/com/nt/ntp/exception/InvalidGreenGoodsOfFactoryException.java
+++ src/main/java/com/nt/ntp/exception/InvalidGreenGoodsOfFactoryException.java
@@ -3,6 +3,6 @@ package com.nt.ntp.exception;
 public class InvalidGreenGoodsOfFactoryException extends RuntimeException{
 
     public InvalidGreenGoodsOfFactoryException() {
-        super("GreenGoodsFactory 에 할당된 GreenGoods 가 할당되지 않았거나 유효하지 않습니다.");
+        super("GreenGoodsFactory 에 할당된 GreenGoods 가 null 이거나 유효하지 않습니다.");
     }
 }
